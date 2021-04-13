38cf64eb05a653e734d42e36b4b4af2c9b451779
meghanvl <meghanvluoma@gmail.com>
2021-04-13 16:39:09 -0500

    adding sqlite database

--- flask/app.py
+++ flask/app.py
@@ -1,25 +1,11 @@
+import numpy as np
 import sqlalchemy
-import os
+import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-from flask import (
-    Flask,
-    render_template,
-    jsonify,
-    request,
-    redirect)
 
-app = Flask(__name__)
-
-from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///ORD_Delays.sqlite"
-
-# Remove tracking modifications
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
+from flask import Flask, jsonify, render_template
 
 engine = create_engine("sqlite:///ORD_Delays.sqlite")
 
@@ -27,5 +13,28 @@ Base = automap_base()
 
 Base.prepare(engine, reflect=True)
 
-Delays = Base.classes.Delays
+Delays = Base.classes.delays
 
+app = Flask(__name__)
+
+@app.route("/test")
+def test():
+    
+    session = Session(engine)
+   
+    results = session.query(Delays.FL_DATE, Delays.ORIGIN, Delays.DEP_DELAY).all()
+    
+    session.close()
+    
+    delays = []
+    for row in results:
+        delay_dict = {}
+        delay_dict["Date"] = row.FL_DATE 
+        delay_dict["Origin"] = row.ORIGIN
+        delay_dict["Delay Duration"] = row.DEP_DELAY   
+        delays.append(delay_dict)
+
+    return jsonify(delays)
+
+if __name__ == "__main__":
+    app.run(debug=True)
