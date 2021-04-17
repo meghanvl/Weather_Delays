b4e6ec4c23e8c25fa08ed83958a6b8bdba739c84
meghanvl <meghanvluoma@gmail.com>
2021-04-16 20:13:17 -0500

    moved css and html files

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import sqlalchemy
-import datetime as dt
+import os
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
 
-from flask import Flask, jsonify, render_template
+app = Flask(__name__)
+
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///ORD_Delays.sqlite"
+
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 engine = create_engine("sqlite:///ORD_Delays.sqlite")
 
@@ -13,8 +27,6 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 Delays = Base.classes.ordtable
-
-app = Flask(__name__)
 
 @app.route("/")
 def index():
